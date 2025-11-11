8eb4ecf66f8229a202d8e79e6ba39699c53f5a1a
hrnicek <hrncir39@gmail.com>
2025-11-11 16:14:22 +0100

    init project

---
+++ resources/js/app.js
@@ -0,0 +1,28 @@
+import './bootstrap';
+import '../css/app.css';
+
+import { createApp, h } from 'vue';
+import { createInertiaApp } from '@inertiajs/vue3';
+import { resolvePageComponent } from 'laravel-vite-plugin/inertia-helpers';
+import { ZiggyVue } from '../../vendor/tightenco/ziggy';
+
+const appName = import.meta.env.VITE_APP_NAME || 'Laravel';
+
+import WebLayout from './layouts/WebLayout.vue';    
+import { Link } from '@inertiajs/vue3';
+
+createInertiaApp({
+    title: (title) => `${title} - ${appName}`,
+    resolve: (name) => resolvePageComponent(`./pages/${name}.vue`, import.meta.glob('./pages/**/*.vue')),
+    setup({ el, App, props, plugin }) {
+        return createApp({ render: () => h(App, props) })
+            .use(plugin)
+            .use(ZiggyVue)
+            .component('WebLayout', WebLayout)
+            .component('Link', Link)
+            .mount(el);
+    },
+    progress: {
+        color: '#4B5563',
+    },
+});
---
+++ resources/js/pages/Home.vue
@@ -0,0 +1,10 @@
+<script setup lang="ts">
+import WebLayout from '../layouts/WebLayout.vue';
+
+</script>
+
+<template>
+  <WebLayout>
+    
+  </WebLayout>
+</template>
