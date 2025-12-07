831e0fe884def9105cca75b0cc83517c071ee6e5
hrnicek <hrncir39@gmail.com>
2025-12-07 09:13:57 +0100

    update

--- resources/js/components/Hero.vue
+++ resources/js/components/Hero.vue
@@ -1,25 +1,28 @@
 <script setup>
+import { onMounted, ref } from 'vue';
 import HeroContent from "./HeroContent.vue";
-import { Swiper, SwiperSlide } from 'swiper/vue';
-import { Autoplay } from 'swiper/modules';
-import 'swiper/css';
+import gsap from 'gsap';
 
-const images = ['/img/hero.webp', '/img/hero2.webp', '/img/hero3.webp', '/img/hero4.webp'];
+const heroImage = ref(null);
+
+onMounted(() => {
+  gsap.fromTo(heroImage.value, 
+    { scale: 1 }, 
+    { scale: 1.1, duration: 20, ease: "none", repeat: -1, yoyo: true }
+  );
+});
 </script>
 
 <template>
   <div class="hero-font bg-gray-900 text-white">
     <div class="relative min-h-screen w-full overflow-hidden">
       <div class="absolute inset-0 z-0">
-        <Swiper :modules="[Autoplay]" :autoplay="{ delay: 5000, disableOnInteraction: false }" loop class="h-full w-full">
-          <SwiperSlide v-for="image in images" :key="image">
-            <img
-              :src="image"
-              alt="Luxusní horská chata s wellness za západu slunce"
-              class="h-full w-full object-cover"
-            />
-          </SwiperSlide>
-        </Swiper>
+        <img
+          ref="heroImage"
+          src="/img/hero.webp"
+          alt="Luxusní horská chata s wellness za západu slunce"
+          class="h-full w-full object-cover"
+        />
         <div class="absolute inset-0 bg-black/40"></div>
       </div>
 
